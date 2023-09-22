a733a12fc7be9301ddeb32b668b3c7d7323f2123
Dido <100509456+Dido2002@users.noreply.github.com>
2023-09-22 22:24:19 +0300

    Add files via upload

---
+++ App03.js
@@ -0,0 +1,15 @@
+// 3. Random цитати
+// Създайте компонент Quotes, генериращ случаен цитат от масив през 10 секунди.
+// Цитатите са предварително дефинирани в масив.
+
+
+import React from "react";
+import Quotes from "./Quotes";
+
+export default function App03(){
+    return(
+        <div>
+            <Quotes/>
+        </div>
+    );
+}
