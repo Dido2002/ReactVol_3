54b63efbc86f5904f8db118d2d3ed1eb9a79f1a1
Dido <100509456+Dido2002@users.noreply.github.com>
2023-09-22 22:23:44 +0300

    Add files via upload

---
+++ App01.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from '../pages/home/Home';
+import About from '../pages/about/About';
+
+
+// . Създаване на Layout
+// Създайте Layout с компонент Header, Menu, Footer и страница Home и я използвайте в
+// App компонента
+
+export default function App01(){
+    return(
+        <div>
+            <Home/>
+        </div>
+    )
+
+}
