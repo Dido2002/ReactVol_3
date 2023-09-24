a5d5b87d926d4e13f1114943f1303d0b346972c1
Dido <100509456+Dido2002@users.noreply.github.com>
2023-09-24 18:54:22 +0300

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Header.css";
+
+// function Header(){
+//     return(
+//         <header>
+//             <h1></h1>
+//         </header>
+//     )
+// }
+// export default Header;
+
+const Header = () => {
+    return(
+        <header>
+            <h1>This is header</h1>
+        </header>
+    );
+}
+export default Header;
