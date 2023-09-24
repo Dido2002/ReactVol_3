00971e3a1362d9886741bd5e8821a8686d68c412
Dido <100509456+Dido2002@users.noreply.github.com>
2023-09-24 18:53:39 +0300

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const styles = {
+    background: "#333",
+    color:"white",
+    padding:"20px",
+    textAling:"center"
+}
+
+export default () => <footer style={styles}>&copy;Blacatz Academy 2023</footer>
