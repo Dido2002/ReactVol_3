38448a84597c63b9115ed9481067ec8749b8786d
Dido <100509456+Dido2002@users.noreply.github.com>
2023-09-24 18:54:45 +0300

    Add files via upload

---
+++ Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Menu.css";
+
+const Menu = () => {
+    return(
+        <nav>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Contacts</a>
+        </nav>
+    )
+};
+export default Menu;
